052a501ce6ca2ced2b5ffd73b1e10cfd82c7fa85
Gayathri Gireesh Sujatha <gayathri.gireeshsujatha@nielsen.com>
2025-07-17 22:12:08 +0530

    env

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,2 +1,2 @@
-export const SHEETDB_ENDPOINT = 'https://sheetdb.io/api/v1/cb6vb83c7qooo';
+export const SHEETDB_ENDPOINT = process.env.REACT_APP_SHEETDB_ENDPOINT;
 export const ICON_SIZE = 'w-10 h-10'; 
