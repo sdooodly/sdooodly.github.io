e58e9528c215529c6408271c8e9a3c5159415b25
Gayathri Gireesh Sujatha <gayathri.gireeshsujatha@nielsen.com>
2025-07-18 14:29:33 +0530

    remove logs

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,4 +1,2 @@
 export const SHEETDB_ENDPOINT = process.env.REACT_APP_SHEETDB_ENDPOINT;
-console.log('TEST VAR:', process.env.REACT_APP_TEST_VAR);
-console.log('SHEETDB_ENDPOINT:', SHEETDB_ENDPOINT);
 export const ICON_SIZE = 'w-10 h-10'; 
